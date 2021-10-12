446281372c229232ae2b7b039fedbcc9acde793d
fini04001 <92377261+fini04001@users.noreply.github.com>
2021-10-12 15:29:07 +0200

    Add files via upload

---
+++ Jämna tal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jämna_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
